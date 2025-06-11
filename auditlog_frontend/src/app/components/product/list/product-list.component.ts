4a0f238f8af251f66b1b36fdec3dad48ff52bd0b
cong34dk <nguyencong12g2003@gmail.com>
2025-06-11 16:58:03 +0700

    add product-list

--- auditlog_frontend/src/app/components/product/list/product-list.component.ts
+++ auditlog_frontend/src/app/components/product/list/product-list.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../models/product';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
+  totalRecord: number = 0;
+  pageSize: number = 10;
+  searchTerm: string = '';
   
   constructor(private _productService: ProductService, private router: Router) {}
 
@@ -21,9 +27,12 @@ export class ProductListComponent implements OnInit {
     this.loadProducts();
   }
   
-  loadProducts() {
-    this._productService.getProducts().subscribe((res: any) => {
+  loadProducts(page: number = 1): void {
+    this._productService.getProducts(this.searchTerm, page, this.pageSize).subscribe((res: any) => {
       this.products = res.products;
+      this.currentPage = res.currentPage;
+      this.totalPages = res.totalPages;
+      this.totalRecord = res.totalRecord;
     });
   }
 
@@ -41,5 +50,25 @@ export class ProductListComponent implements OnInit {
         this.loadProducts();
       });
     }
+  }
+
+  changePage(page: number): void {
+    if (page > 1 && page <= this.totalPages) {
+      this.loadProducts(page);
+    }
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  onSearch(): void {
+    this.currentPage = 1;
+    this.loadProducts();
+  }
+
+  onPageSizeChange(): void {
+    this.currentPage = 1;
+    this.loadProducts();
   }
 }
